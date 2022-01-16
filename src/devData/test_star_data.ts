f41d9b12b3118238636b0a6c492a4cecfe1236cf
Sam <93292288+Earthsam12@users.noreply.github.com>
2022-01-16 11:57:40 -0500

    feat: better test star data

--- src/devData/test_star_data.ts
+++ src/devData/test_star_data.ts
@@ -1,42 +1,47 @@
+import { Star } from "../Classes/Star"
 
-// // FOR ENDPOINTPIVOT
-// export const starName = "TRUPS4";
-// export const pointNames = ['RANJR', 'JARHD', 'PJAAE', 'USAAY', 'WEEDU', 'SUPRT', 'OOURR', 'TRUPS'];
-// export const legLengths = [9, 9, 12, 7, 8, 13, 8];
-// export const constraints = [
-//     [28000, 24000],
-//     [26000, 22000],
-//     [23000, 21000],
-//     [21000, 17000],
-//     [19000, 16000],
-//     [17000, 14000],
-//     [13000, 0],
-//     [11000, 11000]];
+export const TRUPS4 = new Star(
+    "TRUPS4",
+    ['RANJR', 'JARHD', 'PJAAE', 'USAAY', 'WEEDU', 'SUPRT', 'OOURR', 'TRUPS'],
+    [
+        [28000, 24000],
+        [26000, 22000],
+        [23000, 21000],
+        [21000, 17000],
+        [19000, 16000],
+        [17000, 14000],
+        [13000, 0],
+        [11000, 11000]
+    ],
+    [9, 9, 12, 7, 8, 13, 8]
+)
 
-// FOR RIGIDCHECK
-// export const starName = "NITZR3";
-// export const pointNames = ['KBEEE', 'DEMLL', 'NITZR', 'WRSAW', 'DAHRL', 'GDNEE', 'ELLKO', 'SAVVG', 'GREAK'];
-// export const legLengths = [28, 13, 4, 8, 10, 6, 6, 8];
-// export const constraints = [
-//     [100000, 24000],
-//     [100000, 16000],
-//     [100000, 12000],
-//     [100000, 11000],
-//     [100000, 0],
-//     [100000, 0],
-//     [100000, 11000],
-//     [10000, 10000],
-//     [8000, 8000]
-// ];
+export const NITZR3 = new Star(
+    "NITZR3",
+    ['KBEEE', 'DEMLL', 'NITZR', 'WRSAW', 'DAHRL', 'GDNEE', 'ELLKO', 'SAVVG', 'GREAK'],
+    [
+        [100000, 24000],
+        [100000, 16000],
+        [100000, 12000],
+        [100000, 11000],
+        [100000, 0],
+        [100000, 0],
+        [100000, 11000],
+        [10000, 10000],
+        [8000, 8000]
+    ],
+    [28, 13, 4, 8, 10, 6, 6, 8]
+)
 
-// (FAKE) FOR PIVOT
-// export const starName = "FAKEE1";
-// export const pointNames = ['AAAAA', 'BBBBB', 'CCCCC','DDDDD', 'EEEEE'];
-// export const legLengths = [5,6,5,6];
-// export const constraints = [
-//     [100000,15000],
-//     [100000,13000],
-//     [12000,12000],
-//     [12000,0],
-//     [10000,0]
-// ];
+export const FAKEE1 = new Star(
+    "FAKEE1",
+    ['AAAAA', 'BBBBB', 'CCCCC', 'DDDDD', 'EEEEE'],
+    [
+        [100000, 15000],
+        [100000, 13000],
+        [12000, 12000],
+        [12000, 0],
+        [10000, 0]
+    ],
+    [5, 6, 5, 6]
+)
